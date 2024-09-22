d2cf9a6da431094a1cf5e437d1b9342c570eb8b6
edenverjolas <edenverjolas@gmail.com>
2024-09-22 14:18:29 +0000

    check user updated

--- bash/checkuser.sh
+++ bash/checkuser.sh
@@ -3,12 +3,28 @@
 ######################################################################################
 
 #!/bin/bash
-echo "Hello, here we read a username and decide if its an existing user in this system"
-read -r name1
-if (( $(cat /etc/passwd | cut -d':' -f 1 | grep -x $name1 | wc -l) > "0" )); then
-  echo "This username exists in this system, goodbye now"
-else
-  echo "This username does not exist in this system, goodbye now"
-fi
-read
+
+check-user() {
+	echo "Please enter a username to check whether it exists in this system"
+	read -r name1
+	if (($(cat /etc/passwd | cut -d':' -f 1 | grep -x $name1 | wc -l) > "0")); then
+		echo "This username exists in this system"
+	else
+		echo "This username does not exist in this system"
+	fi
+	read
+}
+while true; do
+	clear
+	echo -e "Username search, proceed ? y/n"
+	read -rn1 choice1
+	if [[ "$choice1" == "y" ]]; then
+		clear
+		check-user
+	else
+		echo -e "Goodbye now, thanks"
+		clear
+		break
+	fi
+done
 exit
